36478e4799665cca1acb37778e08a954aece244c
JiratchayaPhinyodom <jiratchaya.phiny@ku.th>
2023-10-21 11:14:32 +0700

    MAke the condition that if the system have data of the user its will display profile photo, email and name on the screeen

--- src/signIn.jsx
+++ src/signIn.jsx
@@ -22,20 +22,35 @@ function SignIn() {
 
   return (
     <div className="sec2">
-      <div className='img_sec2'>
-        <img src={Olympic} width="500" height="230.83" className='img_olympic_logo' ></img>
-        <h1 className='topics'>Paris 2024</h1>
-        <h1 className='topics'>Olympic Games</h1>
-      </div>
-      <div className='sec1'>
-              <p className='topic_login'>LOGIN</p>
-            <button className='btn-login'
-              onClick={handleGoogleSignIn}>
-              <p className='parah'>Sign In With Google</p>
+      {user ? (
+        <>
+          <button className="btn btn-secondary btn-md" onClick={handleLogout}>
+            LOGOUT
+          </button>
+          <h3>Welcome {user.displayName}</h3>
+          <p>{user.email}</p>
+          <div className="photo">
+            <img src={user.photoURL} alt="dp" referrerPolicy="no-referrer" />
+          </div>
+        </>
+      ) : (
+        <>
+          <div className="img_sec2">
+            <img src={Olympic} width="500" height="230.83" className="img_olympic_logo" alt="Olympic Logo" />
+            <h1 className="topics">Paris 2024</h1>
+            <h1 className="topics">Olympic Games</h1>
+          </div>
+          <div className="sec1">
+            <p className="topic_login">LOGIN</p>
+            <button className="btn-login" onClick={handleGoogleSignIn}>
+              <p className="parah">Sign In With Google</p>
             </button>
-      </div>
+          </div>
+        </>
+      )}
     </div>
   );
+  
 }
 
 export default SignIn;
