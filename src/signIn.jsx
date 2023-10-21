fc51b130a89e9dc9e77946d3e5ead64893cb45e2
JiratchayaPhinyodom <jiratchaya.phiny@ku.th>
2023-10-21 11:00:50 +0700

    More decorate in the login page and have some a detail to fix.

--- src/signIn.jsx
+++ src/signIn.jsx
@@ -2,6 +2,7 @@ import {auth, provider} from './FirebaseConfig';
 import {useState} from 'react';
 import { signInWithPopup } from 'firebase/auth';
 import img_sec2 from './img/sec2.png'
+import Olympic from './img/Olympic.png'
 function SignIn() {
 
   const [user, setUser] = useState(null);
@@ -21,17 +22,18 @@ function SignIn() {
   }
 
   return (
-    <div className="wrapper">
+    <div className="sec2">
       <div className='img_sec2'>
-        <img src={img_sec2} width="650" height="944"></img>
+        <img src={Olympic} width="500" height="230.83" className='img_olympic_logo' ></img>
+        <h1 className='topics'>Paris 2024</h1>
+        <h1 className='topics'>Olympic Games</h1>
       </div>
-      <div className='box'>
-            <div className='section1'>
+      <div className='sec1'>
               <p className='topic_login'>LOGIN</p>
             <button className='btn-login'
               onClick={handleGoogleSignIn}>
               <p className='parah'>Sign In With Google</p>
-            </button> </div> 
+            </button>
       </div>
     </div>
   );
