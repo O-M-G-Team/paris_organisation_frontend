d4d53ebd13a09273bb8b4449c750bfdcae552e42
Ratthicha Parinthip <92798644+premepreme@users.noreply.github.com>
2023-10-23 00:09:45 +0700

    add add medal and delete button

--- src/pages/Result.jsx
+++ src/pages/Result.jsx
@@ -2,16 +2,19 @@
 // import Dropdown from '../components/dropdown.jsx'
 import '../styles/dropdown.css'
 import Card from '../components/card';
+import Add from '../components/addbutton'
 
 const Result = () => {
   return (
+    <>
+    <Add/>
     <div className="dropdown1">
         <h1></h1>
         <Card/>
         <Card/>
         <Card/>
     </div>
-
+</>
   )
 }
 
