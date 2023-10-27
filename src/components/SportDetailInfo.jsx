31bbd5696cefaf698cbd5fc321dd32267b8820f5
Siripa-Maneein <siripa.m@ku.th>
2023-10-28 02:32:03 +0700

    add sport detail css

--- src/components/SportDetailInfo.jsx
+++ src/components/SportDetailInfo.jsx
@@ -5,38 +5,56 @@ import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-
+import '../styles/SportDetail.css'
+import { format } from 'date-fns';
 
 function SportDetailInfo(props) {
-    console.log(props.sport_detail.result)
     const result = props.sport_detail.result;
+
 
     return (
         <div style={{ padding: 5 }} >
-            <Card sx={{ minWidth: 275, b: 10 }}>
+            <Card sx={{ minWidth: 500, b: 50 }}>
                 <CardContent>
-                    <Typography sx={{ fontSize: 30 }} color="div" gutterBottom>
+                    <h1>
                         {props.sport_detail.sport_name}
-                    </Typography>
-                    <Typography variant="h6" >
+                    </h1>
+                    <h3>
                         Participating Country <br></br>
-                    </Typography>
-                    <Typography variant="p" >
-                        {props.sport_detail.participating_country ? props.sport_detail.participating_country.join(', ') : ''}
-                    </Typography>
-                    <Typography variant="h6">
+                    </h3>
+                    <p>
+                        &emsp;{props.sport_detail.participating_country ? props.sport_detail.participating_country.join(', ') : ''}
+                    </p>
+                    <h3>
                         Date of the match <br></br>
-                    </Typography>
-                    <Typography variant="p">
-                        {props.sport_detail.date_time}
-                    </Typography>
-
+                    </h3>
+                    <p>
+                        &emsp;{props.sport_detail.date_time}
+                    </p>
+                    <h3>
+                        Result<br></br>
+                    </h3>
+                    
                     <table>
+                        
+                        <thead>
+                            <tr>
+                                <th>Country</th>
+                                <th>Medal</th>
+                            </tr>
+                        </thead>
                         <tbody>
-                            {result && Object.entries(result).map(([key, value]) => (<tr key={key}>
-                                <td>{key}</td>
-                                <td>{value}</td>
-                            </tr>))}
+                        {result && Object.keys(result).length > 0
+                            ? Object.entries(result).map(([key, value]) => (
+                                <tr key={key}>
+                                    <td>{key}</td>
+                                    <td>{value}</td>
+                                </tr>
+                            ))
+                            : <tr>
+                                <td colSpan="2"> &emsp; No result available</td>
+                            </tr>
+                        }
                         </tbody>
                     </table>
                 </CardContent>
