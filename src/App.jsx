7300bc6a16be5fb50c06acc97b73b5cab01a1165
Siripa-Maneein <siripa.m@ku.th>
2023-10-27 14:56:33 +0700

    add sport detail component

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {Routes, Route, useNavigate} from 'react-router-dom';
+import SportDetail from './pages/SportDetail';
 
-function App() {
-  const [count, setCount] = useState(0)
+
+export default function App() {
+  const navigate = useNavigate();
+
+  const navigateToSportDetail = (sport_id) => {
+    // 👇️ navigate to /sport_detail
+    navigate(`/sport_detail/${sport_id}`);
+  };
+
 
   return (
-    <>
+    <div>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <button onClick={() => navigateToSportDetail('3')}>A button in table</button>
+
+        <Routes>
+          <Route path="/sport_detail/:sport_id" element={<SportDetail />} />
+        </Routes>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+function Home() {
+  return <h2>Home</h2>;
+}
+
+
