2a77f1d0a2681a235ebe0d3bc0e5c28280aed362
Siripa-Maneein <siripa.m@ku.th>
2023-10-28 11:36:03 +0700

    improve overall appearance

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,12 @@
-import {Routes, Route, useNavigate} from 'react-router-dom';
+import {Routes, Route} from 'react-router-dom';
 import SportDetail from './pages/SportDetail';
 
 
 export default function App() {
-  const navigate = useNavigate();
-
-  const navigateToSportDetail = (sport_id) => {
-    // 👇️ navigate to /sport_detail
-    navigate(`/sport_detail/${sport_id}`);
-  };
-
 
   return (
     <div>
       <div>
-        <button onClick={() => navigateToSportDetail('3')}>A button in table</button>
 
         <Routes>
           <Route path="/sport_detail/:sport_id" element={<SportDetail />} />
@@ -24,8 +16,5 @@ export default function App() {
   );
 }
 
-function Home() {
-  return <h2>Home</h2>;
-}
 
 
--- src/components/SportDetailInfo.jsx
+++ src/components/SportDetailInfo.jsx
@@ -1,62 +1,84 @@
 import React from 'react'
 
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
 import '../styles/SportDetail.css'
-import { format } from 'date-fns';
 
 function SportDetailInfo(props) {
     const result = props.sport_detail.result;
+    const date = props.sport_detail.date_time;
+
+    const options = {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+        hour: '2-digit',
+        minute: '2-digit'
+      };
+
+    const formattedDate = new Date(date).toLocaleString(undefined, options);
+
+    const styles = {
+        container: {
+          display: 'flex',
+          flexDirection: 'row',
+          height: '100vh',
+        },
+        card: {
+          minWidth: 500
+          
+        },
+      };
 
 
     return (
-        <div style={{ padding: 5 }} >
-            <Card sx={{ minWidth: 500, b: 50 }}>
+        <div style={styles.container} >
+            <Card sx={styles.card}>
                 <CardContent>
                     <h1>
                         {props.sport_detail.sport_name}
                     </h1>
-                    <h3>
+                    <h2>
                         Participating Country <br></br>
-                    </h3>
+                    </h2>
                     <p>
                         &emsp;{props.sport_detail.participating_country ? props.sport_detail.participating_country.join(', ') : ''}
                     </p>
-                    <h3>
+                    <br></br>
+                    <h2>
                         Date of the match <br></br>
-                    </h3>
+                    </h2>
                     <p>
-                        &emsp;{props.sport_detail.date_time}
+                        &emsp;{formattedDate}
                     </p>
-                    <h3>
+                    <br></br>
+                    <h2>
                         Result<br></br>
-                    </h3>
-                    
-                    <table>
-                        
-                        <thead>
-                            <tr>
-                                <th>Country</th>
-                                <th>Medal</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                        {result && Object.keys(result).length > 0
-                            ? Object.entries(result).map(([key, value]) => (
-                                <tr key={key}>
-                                    <td>{key}</td>
-                                    <td>{value}</td>
+                    </h2>
+                    <div style={{marginLeft:'1rem'}}>
+                        <table>
+                            
+                            <thead>
+                                <tr>
+                                    <th>Country</th>
+                                    <th>Medal</th>
                                 </tr>
-                            ))
-                            : <tr>
-                                <td colSpan="2"> &emsp; No result available</td>
-                            </tr>
-                        }
-                        </tbody>
-                    </table>
+                            </thead>
+                            <tbody>
+                            {result && Object.keys(result).length > 0
+                                ? Object.entries(result).map(([key, value]) => (
+                                    <tr key={key}>
+                                        <td>{key}</td>
+                                        <td>{value}</td>
+                                    </tr>
+                                ))
+                                : <tr>
+                                    <td colSpan="2"> &emsp; No result available</td>
+                                </tr>
+                            }
+                            </tbody>
+                        </table>
+                    </div>
                 </CardContent>
             </Card>
         </div>
