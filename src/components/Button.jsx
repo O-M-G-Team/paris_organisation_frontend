0c0dfc5561d0c122d9f47020ad8be9835682743d
Ratthicha Parinthip <92798644+premepreme@users.noreply.github.com>
2023-10-31 21:34:42 +0700

    add submit button and fix add medal button

---
+++ src/components/Button.jsx
@@ -0,0 +1,9 @@
+const Button = ({ name, type, onClick }) => {
+    return (
+        <button className="button" type={type} onClick={onClick}>
+            {name}
+        </button>
+    )
+}
+
+export default Button
--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,17 +1,60 @@
 import '../styles/card.css'
-import { useState } from 'react'
-import Medal from '../components/medal.jsx'
-import Country from '..//components/dropdown.jsx'
+import { useState } from 'react';
+import Medal from '../components/Medal';
+import Country from '../components/Dropdown';
+import Button from './Button';
 
 const Card = () => {
-    const [selected, setSelected] = useState("Select Medal");
-    const [selected2, setSelecte2] = useState("Select country");
-  return (
-    <div className='card'>
-        <Medal selected={selected} setSelected={setSelected}/>
-        <Country selected={selected2} setSelected={setSelecte2}/>
-     </div>
-  )
-}
+  const [entries, setEntries] = useState([
+    { medal: 'Select Medal', country: 'Select Country' },
+    { medal: 'Select Medal', country: 'Select Country' },
+    { medal: 'Select Medal', country: 'Select Country' }
+  ]);
 
-export default Card
+  const addEntry = () => {
+    setEntries([...entries, { medal: 'Select Medal', country: 'Select Country' }]);
+  };
+
+  const deleteEntry = (index) => {
+    const updatedEntries = [...entries];
+    updatedEntries.splice(index, 1);
+    setEntries(updatedEntries);
+  };
+
+  const handleEntryChange = (index, field, value) => {
+    const updatedEntries = [...entries];
+    updatedEntries[index][field] = value;
+    setEntries(updatedEntries);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(entries);
+  };
+
+  return (
+    <>
+            <Button name="Add" onClick={addEntry} />
+      <form className="place-form" onSubmit={handleSubmit}>
+        {entries.map((entry, index) => (
+          <div className="card" key={index}>
+            <Medal
+              selected={entry.medal}
+              setSelected={(value) => handleEntryChange(index, 'medal', value)}
+            />
+            <Country
+              selected={entry.country}
+              setSelected={(value) => handleEntryChange(index, 'country', value)}
+            />
+            {index >= 3 && ( // Only show delete button for dynamically added cards
+              <Button name="Delete" onClick={() => deleteEntry(index)} />
+            )}
+          </div>
+        ))}
+        <Button name="Submit" type="submit" />
+      </form>
+    </>
+  );
+};
+
+export default Card;
