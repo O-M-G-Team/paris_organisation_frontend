8f1aaec2655a1de6824850e45fe63d8a64620297
JiratchayaPhinyodom <jiratchaya.phiny@ku.th>
2023-10-31 21:16:27 +0700

    Finished control user to sign in before go to anotheer page but have problem issue on sport table #4

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,32 @@
+import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import SignIn from "./signIn";
-import MainRouter from "./Route/Route"
-import { AuthProvider, useAuth } from "./Route/AuthContext";
-import "./signIn.css"
-import SportTable from './pages/SportTable'
+import { AuthProvider, useAuth } from './firebase/AuthContext'; // Import your authentication context
+import SignIn from "./pages/signIn";
+import "./styles/signIn.css";
+import SportTable from './pages/SportTable';
+import Home from './Home';
 
-const MainApp  = ()=> {
-  const {currentUser} = useAuth();
-  if (currentUser === null){
-    console.log("Execute null")
-    return <SignIn/>
-  }
-  else{
+const MainRouter = () => (
+  <Routes>
+    <Route path="/home" element={<Home />} />
+    <Route path="/table" element={<SportTable />} />
+  </Routes>
+);
+
+const MainApp = () => {
+  const { currentUser } = useAuth();
+
+  if (currentUser === null) {
+    console.log("Execute null");
+    return <SignIn />;
+  } else {
     console.log("Main Router");
-    console.log(currentUser)
-    console.log(currentUser.uid)
-    return <MainRouter/>
-  };
-}
+    console.log(currentUser);
+    console.log(currentUser.uid);
+    return <MainRouter />;
+  }
+};
 
 function App() {
   return (
--- src/pages/signIn.jsx
+++ src/pages/signIn.jsx
@@ -1,7 +1,7 @@
-import {auth, provider} from './FirebaseConfig';
+import {auth, provider} from '../firebase/FirebaseConfig';
 import {useState} from 'react';
 import { signInWithPopup } from 'firebase/auth';
-import Olympic from './img/Olympic.png'
+import Olympic from "../img/Olympic.png"
 
 function SignIn() {
 
@@ -12,6 +12,7 @@ function SignIn() {
       const user = result.user;
       console.log(user);
       setUser(user);
+      window.location.href = "/home";
     }).catch((err)=>{
       // console.log(err);
     })
@@ -24,19 +25,6 @@ function SignIn() {
 
   return (
     <div className="sec2">
-      {user ? (
-        <>
-          <button className="btn btn-secondary btn-md" onClick={handleLogout}>
-            LOGOUT
-          </button>
-          <h3>Welcome {user.displayName}</h3>
-          <p>{user.email}</p>
-          <div className="photo">
-            <img src={user.photoURL} alt="dp" referrerPolicy="no-referrer" />
-          </div>
-        </>
-      ) : (
-        <>
           <div className="img_sec2">
             <img src={Olympic} width="500" height="230.83" className="img_olympic_logo" alt="Olympic Logo" />
             <h1 className="topics">Paris 2024</h1>
@@ -48,8 +36,6 @@ function SignIn() {
               <p className="parah">Sign In With Google</p>
             </button>
           </div>
-        </>
-      )}
     </div>
   );
   
