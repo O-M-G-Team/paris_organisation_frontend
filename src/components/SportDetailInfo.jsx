94f34807d564b476b0abaedb96e93c48a9d2bc6a
Siripa-Maneein <siripa.m@ku.th>
2023-11-11 15:04:16 +0700

    switch column medal and country

--- src/components/SportDetailInfo.jsx
+++ src/components/SportDetailInfo.jsx
@@ -27,54 +27,52 @@ function SportDetailInfo(props) {
 
     return (
         <div>
-            <Card sx={{ width: '500px', height:'400px' }}>
-                <CardContent>
-                    <h1>
-                        {props.sport_detail.sport_name}
-                    </h1>
-                    <h2>
-                        Participating Country <br></br>
-                    </h2>
-                    <p>
-                        &emsp;{props.sport_detail.participating_country ? props.sport_detail.participating_country.join(', ') : 'No country available'}
-                    </p>
-                    <br></br>
-                    <h2>
-                        Date of the match <br></br>
-                    </h2>
-                    <p>
-                        &emsp;{formattedDate}
-                    </p>
-                    <br></br>
-                    <h2>
-                        Result<br></br>
-                    </h2>
-                    <div className="detail" style={{marginLeft:'1rem'}}>
-                        <table>
-                            
-                            <thead>
-                                <tr>
-                                    <th>Country</th>
-                                    <th>Medal</th>
+            <br></br>
+            <h1>
+                {props.sport_detail.sport_name}
+            </h1>
+            <br></br>
+            <h2>
+                Participating Country <br></br>
+            </h2>
+            <p>
+                &emsp;{props.sport_detail.participating_country ? props.sport_detail.participating_country.join(', ') : 'No country available'}
+            </p>
+            <br></br>
+            <h2>
+                Date of the match <br></br>
+            </h2>
+            <p>
+                &emsp;{formattedDate}
+            </p>
+            <br></br>
+            <h2>
+                Result<br></br>
+            </h2>
+            <div className="detail" style={{ marginLeft: '1rem' }}>
+                <table>
+
+                    <thead>
+                        <tr>
+                            <th>Medal</th>
+                            <th>Country</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {result && Object.keys(result).length > 0
+                            ? Object.entries(result).map(([key, value]) => (
+                                <tr key={value}>
+                                    <td>{key}</td>
+                                    <td>{value.join(', ')}</td>
                                 </tr>
-                            </thead>
-                            <tbody>
-                            {result && Object.keys(result).length > 0
-                                ? Object.entries(result).map(([key, value]) => (
-                                    <tr key={value}>
-                                        <td>{value}</td>
-                                        <td>{key}</td>
-                                    </tr>
-                                ))
-                                : <tr>
-                                    <td colSpan="2"> &emsp; No result available</td>
-                                </tr>
-                            }
-                            </tbody>
-                        </table>
-                    </div>
-                </CardContent>
-            </Card>
+                            ))
+                            : <tr>
+                                <td colSpan="2"> &emsp; No result available</td>
+                            </tr>
+                        }
+                    </tbody>
+                </table>
+            </div>
         </div>
 
 
