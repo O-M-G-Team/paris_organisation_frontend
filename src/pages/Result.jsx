abf4d6e22f6609ebecae4c6f8c5b8c58d9f21e9a
Ratthicha Parinthip <92798644+premepreme@users.noreply.github.com>
2023-11-17 17:22:24 +0700

    fixed alert and let user know result is save.also fixed style css

--- src/pages/Result.jsx
+++ src/pages/Result.jsx
@@ -54,71 +54,61 @@ const Result = (props) => {
   };
 
   const sendDataToBackend = () => {
-    // const hasMissingInputs = sportResults.some(result => result.medal === 'Select Medal' || result.country === 'Select Country');
-
-    // console.log(sportResults)
-    // console.log(c)
     console.log(sportResults)
-    if (sportResults == 0) {
-      alert('Data is missing. Plase enter all sport result.')
-      window.location.reload();
-    } 
-    else if(sportResults.length == 1){
-      alert('Plase enter all sport result.');
-      sportResults.splice(0, 1);
-      setSportResults([...sportResults]);
-      window.location.reload();
-    }
-    else if(sportResults.length == 2){
-      alert('Plase enter all sport result.');
+    for (let i = 0; i < sportResults.length; i++) {
+      if (sportResults[i].medal === 'Select Medal' && sportResults[i].country === 'Select Country') {
+        console.log('x');
+        alert('Plase enter all sport result.');
       sportResults.splice(0, sportResults.length);
       setSportResults([...sportResults]);
       window.location.reload();
-    }
-    else if (hasDuplicateCountries()) {
+      }}
+      if (hasDuplicateCountries()) {
       alert("Duplicate countries found. Please enter unique countries for each result.");
       sportResults.splice(0, sportResults.length);
       setSportResults([...sportResults]);
-      window.location.reload();
+      window.location.reload();}
 
-    }
-    else {
-    const requestData = {
-      sport_id: 'sport_id1',
-      result: {
-        gold: sportResults
-          .filter((result) => result.medal === 'Gold')
-          .map((result) => result.country),
-        silver: sportResults
-          .filter((result) => result.medal === 'Silver')
-          .map((result) => result.country),
-        bronze: sportResults
-          .filter((result) => result.medal === 'Bronze')
-          .map((result) => result.country),
-      },
-    };
-    console.log(requestData);
-    fetch('http://localhost:8000/paris_org/olympic/enter_result', {
-      method: 'PUT',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(requestData),
-    })
-      .then((response) => {
-        if (response.ok) {
-          return response.json();
-        } else {
-          throw new Error(`Server responded with status: ${response.status}`);
-        }
+      else {
+      const requestData = {
+        sport_id: 'sport_id1',
+        result: {
+          gold: sportResults
+            .filter((result) => result.medal === 'Gold')
+            .map((result) => result.country),
+          silver: sportResults
+            .filter((result) => result.medal === 'Silver')
+            .map((result) => result.country),
+          bronze: sportResults
+            .filter((result) => result.medal === 'Bronze')
+            .map((result) => result.country),
+        },
+      };
+      alert("Result is already saved!");
+      window.location.reload();
+      console.log(requestData);
+      fetch('http://localhost:8000/paris_org/olympic/enter_result', {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(requestData),
       })
-      .catch((error) => {
-        console.error('Error sending data to the backend:', error);
-      });
-  }};
+        .then((response) => {
+          if (response.ok) {
+            return response.json();
+          } else {
+            throw new Error(`Server responded with status: ${response.status}`);
+          }
+        })
+        .catch((error) => {
+          console.error('Error sending data to the backend:', error);
+        });
+    }}
   return (
     <>
-            <div className='add-btt'>
+    <div className='btt-con'>
+    <div className='add-btt'>
         <button onClick={addCard}>Add</button>
         </div>
         <div className='save-btt'>
@@ -129,6 +119,8 @@ const Result = (props) => {
     index > 2 && <button key={index} onClick={() => deleteCard(index)}>Delete</button>
   ))}
 </div>
+    </div>
+
       <div className='resulttable'>
         {/* <div className="header">
           <div className='w'>Enter Result</div>
