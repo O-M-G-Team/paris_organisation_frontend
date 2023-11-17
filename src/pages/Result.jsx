c29468f49bbfc58509325eb8c09aacabdbdf8890
Ratthicha Parinthip <92798644+premepreme@users.noreply.github.com>
2023-11-17 18:00:29 +0700

    fixed alert hasmedalandcountry condition

--- src/pages/Result.jsx
+++ src/pages/Result.jsx
@@ -1,22 +1,21 @@
 // import React from 'react'
 // import Dropdown from '../components/dropdown.jsx'
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import "../styles/header.css";
-import Card from '../components/card';
+import Card from "../components/card";
 import "../styles/button.css";
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
 
 const Result = (props) => {
   const [sportResults, setSportResults] = useState([
-    { medal: 'Select Medal', country: 'Select Country' },
-    { medal: 'Select Medal', country: 'Select Country' },
-    { medal: 'Select Medal', country: 'Select Country' },
+    { medal: "Select Medal", country: "Select Country" },
+    { medal: "Select Medal", country: "Select Country" },
+    { medal: "Select Medal", country: "Select Country" },
   ]);
   const detail = props.sport_detail;
-  const [open,setOpen] = useState(false)
-
+  const [open, setOpen] = useState(false);
 
   const updateSportResults = (index, sportResult) => {
     setSportResults((prevResults) => {
@@ -25,7 +24,7 @@ const Result = (props) => {
       return updatedResults;
     });
   };
-  
+
   const deleteCard = (index) => {
     const updatedResults = [...sportResults];
     updatedResults.splice(index, 1);
@@ -36,14 +35,14 @@ const Result = (props) => {
     // Add a new card with default values
     setSportResults((prevResults) => [
       ...prevResults,
-      { medal: 'Select Medal', country: 'Select Country' },
+      { medal: "Select Medal", country: "Select Country" },
     ]);
   };
 
   const hasDuplicateCountries = () => {
     const countrySet = new Set();
     for (const result of sportResults) {
-      if (result.country !== 'Select Country') {
+      if (result.country !== "Select Country") {
         if (countrySet.has(result.country)) {
           return true; // Duplicate found
         }
@@ -53,44 +52,76 @@ const Result = (props) => {
     return false; // No duplicates found
   };
 
-  const sendDataToBackend = () => {
-    console.log(sportResults)
+  // function hasAllMedals(array) {
+  //   let hasGold = false;
+  //   let hasSilver = false;
+  //   let hasBronze = false;
+
+  //   for (let i = 0; i < array.length; i++) {
+  //     if (array[i].medal === 'Gold') {
+  //       hasGold = true;
+  //     } else if (array[i].medal === 'Silver') {
+  //       hasSilver = true;
+  //     } else if (array[i].medal === 'Bronze') {
+  //       hasBronze = true;
+  //     }
+  //   }
+
+  //   return hasGold && hasSilver && hasBronze;
+  // }
+  // else if (!hasAllMedals(sportResults)){
+  //   alert('Result should contain all three medals (Gold, Silver, and Bronze).');
+  //   sportResults.splice(0, sportResults.length);
+  //   setSportResults([...sportResults]);
+  //   window.location.reload();
+  // }
+
+  function hasMedalAndCountry(array) {
     for (let i = 0; i < sportResults.length; i++) {
-      if (sportResults[i].medal === 'Select Medal' && sportResults[i].country === 'Select Country') {
-        console.log('x');
-        alert('Plase enter all sport result.');
+      if ( array[i].medal === "Select Medal" && array[i].country === "Select Country"
+      ) {
+        return true;
+      } 
+    }
+  }
+
+  const sendDataToBackend = () => {
+    console.log(sportResults);
+    if (hasMedalAndCountry(sportResults)) {
+      console.log(sportResults)
+      alert("Please enter all sport result.");
       sportResults.splice(0, sportResults.length);
       setSportResults([...sportResults]);
       window.location.reload();
-      }}
-      if (hasDuplicateCountries()) {
-      alert("Duplicate countries found. Please enter unique countries for each result.");
+    } else if (hasDuplicateCountries()) {
+      alert(
+        "Duplicate countries found. Please enter unique countries for each result."
+      );
       sportResults.splice(0, sportResults.length);
       setSportResults([...sportResults]);
-      window.location.reload();}
-
-      else {
+      window.location.reload();
+    } else {
       const requestData = {
-        sport_id: 'sport_id1',
+        sport_id: "sport_id1",
         result: {
           gold: sportResults
-            .filter((result) => result.medal === 'Gold')
+            .filter((result) => result.medal === "Gold")
             .map((result) => result.country),
           silver: sportResults
-            .filter((result) => result.medal === 'Silver')
+            .filter((result) => result.medal === "Silver")
             .map((result) => result.country),
           bronze: sportResults
-            .filter((result) => result.medal === 'Bronze')
+            .filter((result) => result.medal === "Bronze")
             .map((result) => result.country),
         },
       };
       alert("Result is already saved!");
       window.location.reload();
       console.log(requestData);
-      fetch('http://localhost:8000/paris_org/olympic/enter_result', {
-        method: 'PUT',
+      fetch("http://localhost:8000/paris_org/olympic/enter_result", {
+        method: "PUT",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify(requestData),
       })
@@ -102,26 +133,32 @@ const Result = (props) => {
           }
         })
         .catch((error) => {
-          console.error('Error sending data to the backend:', error);
+          console.error("Error sending data to the backend:", error);
         });
-    }}
+    }
+  };
   return (
     <>
-    <div className='btt-con'>
-    <div className='add-btt'>
-        <button onClick={addCard}>Add</button>
+      <div className="btt-con">
+        <div className="add-btt">
+          <button onClick={addCard}>Add</button>
         </div>
-        <div className='save-btt'>
+        <div className="save-btt">
           <button onClick={sendDataToBackend}>Save</button>
         </div>
         <div className="delete-btt">
-  {sportResults.map((result, index) => (
-    index > 2 && <button key={index} onClick={() => deleteCard(index)}>Delete</button>
-  ))}
-</div>
-    </div>
+          {sportResults.map(
+            (result, index) =>
+              index > 2 && (
+                <button key={index} onClick={() => deleteCard(index)}>
+                  Delete
+                </button>
+              )
+          )}
+        </div>
+      </div>
 
-      <div className='resulttable'>
+      <div className="resulttable">
         {/* <div className="header">
           <div className='w'>Enter Result</div>
         </div> */}
@@ -129,7 +166,9 @@ const Result = (props) => {
           {sportResults.map((result, index) => (
             <Card
               key={index}
-              updateSportResults={(newResult) => updateSportResults(index, newResult)}
+              updateSportResults={(newResult) =>
+                updateSportResults(index, newResult)
+              }
               countries={detail.participating_country}
               onDelete={() => deleteCard(index)}
             />
