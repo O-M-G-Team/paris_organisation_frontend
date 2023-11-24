ce98e26367bc87cf61edc9b0f47e6d11366a16d5
JiratchayaPhinyodom <jiratchaya.phiny@ku.th>
2023-11-24 14:26:40 +0700

    Fixed mp4 config

--- jest.config.cjs
+++ jest.config.cjs
@@ -6,7 +6,7 @@ module.exports = {
   moduleNameMapper: {
     "^@/(.*)$": "<rootDir>/src/$1",
     '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
-    '\\.(jpg|jpeg|png|gif|webp|svg)$': '<rootDir>/__mocks__/fileMock.js',
+    '\\.(jpg|jpeg|png|gif|webp|svg|mp4)$': '<rootDir>/__mocks__/fileMock.js',
   },
   testEnvironment: "jsdom",
 };
